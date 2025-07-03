65a7cc26542ce2e3dfa7443d61539fec161c6823
LakshyaRaghuwanshi <lakshyawork09@gmail.com>
2025-07-03 18:00:56 +0530

    FindAllFallback method Company constructor error fix

--- job-ms/src/main/java/com/sb_JobApp/job_ms/job/external/Company.java
+++ job-ms/src/main/java/com/sb_JobApp/job_ms/job/external/Company.java
@@ -1,8 +1,12 @@
 package com.sb_JobApp.job_ms.job.external;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Company {
 
     private Long id;
